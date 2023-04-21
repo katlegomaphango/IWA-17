641e7cb25f953ed2f35e9cf375aeb3a50a13d7e3
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-21 17:25:55 +0200

    completed the createData function

--- loops.js
+++ loops.js
@@ -26,12 +26,11 @@ const createArray = (length) => {
 
     return result
 }
-console.log(createArray(5))
 
 const currents = new Date()
-console.log(currents.getDay()) // 5: Fri
+//console.log(currents.getDay()) // 5: Fri
 currents.setDate(1)
-console.log(currents.getDay()) // 6: Sat
+//console.log(currents.getDay()) // 6: Sat
 
 console.log('---------------------')
 
@@ -44,20 +43,21 @@ const createData = () => {
     const startDay = current.getDay() // 6: Sat
     const daysInMonth = getDaysInMonth(current) // 30
 
-    const weeks = createArray(5)
+    const weeks = createArray(6)
     const days = createArray(7)
     let value = null
-
+    let day = 1
     //for (weekIndex in weeks)
     //const [weekIndex, weekDay] of weeks.entries()
     for (let weekIndex = 0; weekIndex < weeks.length; weekIndex++) {
         value = {
-            week: parseInt(weekIndex) + 1,
+            week: `Week ${parseInt(weekIndex) + 1}`,
             days: []
         }
         //console.log(`val ${weekIndex}`,value)
-        let day = 1
-        for (let dayIndex = startDay; dayIndex < days.length; dayIndex++) {
+        
+        for (let dayIndex = 0; dayIndex < days.length; dayIndex++) {
+            /*
             // let day = startDay - dayIndex + 1
             // console.log(day)
             // const isValid = day > 0 && day <= daysInMonth
@@ -66,7 +66,23 @@ const createData = () => {
             //     dayOfWeek: dayIndex + 1,
             //     value: isValid ? day : null,
             // })
-            if(day<=daysInMonth){
+            // console.log(dayIndex)
+            // if(day<=daysInMonth){
+            //     value.days.push({
+            //         dayOfWeek: dayIndex,
+            //         value: day
+            //     })
+            //     day++
+            // }
+            // console.log(value)
+            */
+            if ((dayIndex < startDay && value.week == 'Week 1') || day > daysInMonth){
+                value.days.push({
+                    dayOfWeek: dayIndex,
+                    value: null
+                })
+                
+            } else {
                 value.days.push({
                     dayOfWeek: dayIndex,
                     value: day
@@ -75,12 +91,14 @@ const createData = () => {
             }
         }
 
-        console.log(value)
+        if(day > (daysInMonth+1)) break
+        //console.log(value)
         weeks[weekIndex] = value;
     }
     console.log(weeks)
     return weeks;
 }
+
 
 const addCell = (existing, classString, value) => {
     const result = /* html */ `
@@ -93,10 +111,13 @@ const addCell = (existing, classString, value) => {
 }
 
 const createHtml = (data) => {
-    let result = ''
-
-    // for (week, days in data) {
-    //     let inner = ""
+    let result = 'kat'
+    console.log(data)
+    for (const entry in data) { //for (week, days in data) {
+        console.log(entry)
+        // const [week, days] = entry
+        // console.log(days)
+        let inner = ""
     //     addCell(inner, 'table__cell table__cell_sidebar', 'Week {week}')
     
     //     for (dayOfWeek, value in days) {
@@ -113,8 +134,9 @@ const createHtml = (data) => {
     //         addCell(inner, classString, value)
     //     }
 
-    //     result = `<tr>${inner}</tr>`
-    // }
+        result = `<tr>${inner}</tr>`
+    }
+    return result
 }
 
 // Only edit above
@@ -123,4 +145,42 @@ const current = new Date()
 document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
 
 const data = createData()
+console.log(data)
 document.querySelector('[data-content]').innerHTML = createHtml(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getDaysArray(year, month) {
+//     var numDaysInMonth, daysInWeek, daysIndex, index, i, l, daysArray;
+
+//     //numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//     numDaysInMonth = new Date(year, month, 0).getDate()
+//     daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     daysIndex = { 'Sun': 0, 'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5, 'Sat': 6 };
+//     index = daysIndex[(new Date(year, month - 1, 1)).toString().split(' ')[0]];
+//     console.log(index)
+//     daysArray = [];
+
+//     for (i = 0; i < numDaysInMonth; i++) {
+//         daysArray.push((i + 1) + '. ' + daysInWeek[index++]);
+//         if (index == 7) {
+//             index = 0;
+//         }
+//     }
+
+//     return daysArray;
+// }
+
+// console.log(getDaysArray(new Date().getFullYear(), new Date().getMonth()+1))
